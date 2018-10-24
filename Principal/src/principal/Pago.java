ad4bbf79ffae05561e498875d06bcf3fdf6181f1
lucas zarate <lucasmaxzarate@gmail.com>
2018-10-23 22:49:35 -0300

    Revert "Revert """
    This reverts commit aa8f9cd6e22d35d68bd542d41a7305f571944742.

--- Principal/src/principal/Pago.java
+++ Principal/src/principal/Pago.java
@@ -1,10 +1,10 @@
 
 package principal;
 
-public class Pago { //Aca se va registrar el pago por abono para los afiliados 
-                        //una tarifa por doctor y por grupo familiar 
-                       //que posea el afiliado
-    
+public class Pago { /*Aca se va registrar el pago por abono para los afiliados 
+                        una tarifa por doctor y por grupo familiar 
+                       que posea el afiliado
+                       */
     
     public void registrarPago(){
         
